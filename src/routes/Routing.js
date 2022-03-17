3363f132614c03dd85aed79f44aa94f7c9b97522
SergioGRivera1812 <sergiodidierg@gmail.com>
2022-03-17 17:43:48 -0600

    Sistema de Rutas TERMINADO2.0

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 import { map } from "lodash";
 import configRouting from "./configRouting";
 
 export default function Routing() {
   return (
-      <Router>
-          <Switch>
-              {map(configRouting, (route, index) => (
-                <Route key={index} path={route.path} exact={route.exact}>
-                    <route.page />
-                </Route>
-              ))}
-          </Switch>
-      </Router>
+    <Router>
+      <Routes>
+        {map(configRouting, (route, index) => (
+          <Route key={index} path={route.path} exact={route.exact}>
+            <route.page />
+          </Route>
+        ))}
+      </Routes>
+    </Router>
   );
 }
