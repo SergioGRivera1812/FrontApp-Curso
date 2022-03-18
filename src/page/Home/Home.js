3433d3af060857eef3d08b4b254681d98282185a
SergioGRivera1812 <sergiodidierg@gmail.com>
2022-03-17 18:06:41 -0600

    Sistema Layout Terminado

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import BasicLayout from '../../layout/BasicLayout'
 
 import "./Home.scss"
 
 export default function Home() {
   return (
-    <div>
-        <h2>Estamos en HOME</h2>
-    </div>
-  )
+
+    <BasicLayout>
+      <h2>Estamos en HOME</h2>
+    </BasicLayout>
+
+  );
 }
