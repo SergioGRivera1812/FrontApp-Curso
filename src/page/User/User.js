b0ef687196330376fd43a7987d4dd7283a3f487f
SergioGRivera1812 <sergiodidierg@gmail.com>
2022-04-30 16:56:06 -0500

    ID_UsuarioURL

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Button, Spinner } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
 import BasicLayout from "../../layout/BasicLayout";
 
 import "./User.scss"
 
-export default function User() {
+function User(props) {
+  const { match } = props;
+  console.log(match.params.id);
+
   return (
     <BasicLayout className="user">
-        <div className="user__title">
-            <h2>Pacock</h2>
-        </div>
-        <div>Banner Usuario</div>
-        <div>Info Usuario</div>
-        <div className="user__tweets">Lista Tweets!</div>
+      <div className="user__title">
+        <h2>Pacock</h2>
+      </div>
+      <div>Banner Usuario</div>
+      <div>Info Usuario</div>
+      <div className="user__tweets">Lista Tweets!</div>
     </BasicLayout>
-  )
+  );
 }
+
+export default withRouter(User);
--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { map } from "lodash";
 import configRouting from "./configRouting";
+//import { Switch } from 'react-router-dom';
 
 export default function Routing(props) {
   const { setRefreshCheckLogin } = props;
 
   return (
     <Router>
-      <Switch>
+      <Route>
         {map(configRouting, (route, index) => (
-            <Route key={index} path={route.path} exact={route.exact}>
-            <route.page setRefreshCheckLogin={setRefreshCheckLogin}/>
-            </Route>
+          <Route key={index} path={route.path} exact={route.exact}>
+            <route.page setRefreshCheckLogin={setRefreshCheckLogin} />
+          </Route>
         ))}
-      </Switch>
+      </Route>
     </Router>
   );
 }
